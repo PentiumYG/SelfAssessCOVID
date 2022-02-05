bc8d24e0bee55a3e88c04b81370c6d65478e3700
Yukti Goswami <yuktig.jaipur@gmail.com>
2022-02-05 13:44:02 +0530

    Code updated for main activity.

--- app/src/main/java/com/mc2022/template/MainActivity.java
+++ app/src/main/java/com/mc2022/template/MainActivity.java
@@ -7,29 +7,85 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button nextButton;
+    Button clearButton;
+    Button buttonYes;
+    Button buttonNo;
+    TextView questionText;
+
+    String fever = "Do you suffer from fever?";
+    String runnyNose = "Do you have runny nose?";
+    String scratchyThroat = "Do you suffer from scratchy throat?";
+    String bodyAche = "Do you have body ache?";
+    String headAche = "Do you suffer from head ache?";
+    String looseMotion = "Do you suffer from loose motion?";
+    String tasteSmell = "Are you able to taste and smell?";
+    String submit = "SUBMIT";
+
+    int nextCount=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Fetching buttons ids.
         nextButton = (Button)findViewById(R.id.nextButton);
+        clearButton = (Button)findViewById(R.id.clearButton);
+        buttonNo = (Button)findViewById(R.id.buttonNo);
+        buttonYes = (Button)findViewById(R.id.buttonYes);
+
+        //Fetching TextView id.
+        questionText = (TextView)findViewById(R.id.questionText);
+
+
+
+
 
 
 
         Log.i("create","Activity main is onCreate");
 
-
+        if(nextCount==0){
+            questionText.setText(fever);
+        }
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                nextCount = nextCount + 1;
+                if(nextCount == 1){
+                    questionText.setText(runnyNose);
+                }
+                if(nextCount == 2){
+                    questionText.setText(scratchyThroat);
+                }
+                if(nextCount == 3){
+                    questionText.setText(bodyAche);
+                }
+                if(nextCount == 4){
+                    questionText.setText(headAche);
+                }
+                if(nextCount == 5){
+                    questionText.setText(looseMotion);
+                }
+                if(nextCount == 6){
+                    questionText.setText(tasteSmell);
+                    nextButton.setText(submit);
+
+                }
+                if(nextCount == 7){
+                    Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+                if(nextCount > 7){
+                    nextCount = 0;
+                }
             }
         });
     }
--- app/src/main/java/com/mc2022/template/SecondActivity.java
+++ app/src/main/java/com/mc2022/template/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -17,29 +18,34 @@ public class SecondActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i("start", "Activity second is onStart");
+        Toast.makeText(this, "State of activity main changed from Create to Start", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.i("pause", "Activity second is onPause");
+        Toast.makeText(this, "State of activity main changed from Resume to Pause", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.i("resume", "Activity second is onResume");
+        Toast.makeText(this, "State of activity main changed from Start to Resume", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.i("stop", "Activity second is onStop");
+        Toast.makeText(this, "State of activity main changed from Pause to Stop", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i("destroy", "Activity second is onDestroy");
+        Toast.makeText(this, "State of activity main changed from Stop to Destroy", Toast.LENGTH_SHORT).show();
     }
 }
